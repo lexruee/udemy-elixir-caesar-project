237503dbc58d3fc7ff0ee863d1bb585584703edb
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-09-01 22:02:09 +0200

    Implement escript cli

--- lib/caesar.ex
+++ lib/caesar.ex
@@ -1,18 +1,30 @@
 defmodule Caesar do
-  @moduledoc """
-  Documentation for Caesar.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Caesar.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main(argv) do
+    argv
+    |> parse_args()
+    |> process
+    System.halt(0)
   end
+
+  def parse_args(argv) do 
+    parse = OptionParser.parse(argv, switches: [help: :boolean])
+    case parse do
+      {_, ["help"], _} -> {:help}
+      {[shift: shift], ["encrypt", msg], _} -> {:encrypt, msg, shift |> String.to_integer}
+      {_, ["encrypt", msg], _} -> {:encrypt, msg, 1}
+      _ -> {:help}
+    end
+  end
+
+  def process({:encrypt, msg, shift}) do
+    Caesar.Cipher.encrypt(msg, shift)
+    |> IO.puts()
+  end
+
+  def process({:help}) do
+    IO.puts """
+    usage ./caesar <command> <message> --shift <shift_number>
+    """
+  end
+
 end
